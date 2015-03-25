90044c7a62e3960a2ebdb14b70578f0febcbf1a3
Nathan Arnold <nbarnold01@gmail.com>
2015-03-25 17:54:11 +0700

    Start of NSFetchedResultsController for trip

---
+++ CheckIn/Controllers/TripViewController.swift
@@ -0,0 +1,84 @@
+//
+//  TripViewController.swift
+//  CheckIn
+//
+//  Created by Nathan Arnold on 3/24/15.
+//  Copyright (c) 2015 Nathan Arnold. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class TripViewController: UITableViewController {
+
+    var fetchedResultsController: NSFetchedResultsController {
+        // return if already initialized
+        if self._fetchedResultsController != nil {
+            return self._fetchedResultsController!
+        }
+        let managedObjectContext = self.managedObjectContext!
+        
+        /* `NSFetchRequest` config
+        fetch all `Item`s
+        order them alphabetically by name
+        at least one sort order _is_ required */
+        let entity = NSEntityDescription.entityForName("Item", inManagedObjectContext: managedObjectContext)
+        let sort = NSSortDescriptor(key: "name", ascending: true)
+        let req = NSFetchRequest()
+        req.entity = entity
+        req.sortDescriptors = [sort]
+        
+        /* NSFetchedResultsController initialization
+        a `nil` `sectionNameKeyPath` generates a single section */
+        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: req, managedObjectContext: managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+        aFetchedResultsController.delegate = self
+        self._fetchedResultsController = aFetchedResultsController
+        
+        // perform initial model fetch
+        var e: NSError?
+        if !self._fetchedResultsController!.performFetch(&e) {
+            println("fetch error: \(e!.localizedDescription)")
+            abort();
+        }
+        
+        return self._fetchedResultsController!
+    }
+    
+    var _fetchedResultsController: NSFetchedResultsController?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+//    
+//
+//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+//    
+//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//    
+//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        return 1
+//    }
+//    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
